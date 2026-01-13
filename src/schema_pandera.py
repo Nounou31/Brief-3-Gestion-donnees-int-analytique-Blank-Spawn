d713d2e3d1095f9926cbde9e42cd09f96732ca00
hafida djelti <h.djelti@laposte.net>
2026-01-13 14:28:42 +0100

    Uptade Pandera

--- src/schema_pandera.py
+++ src/schema_pandera.py
@@ -1,6 +1,5 @@
 import pandera as pa
-from pandera import Column, DataFrameSchema, Check
-import pandas as pd
+from pandera import DataFrameSchema, Check
 
 
 # ============================
@@ -9,41 +8,35 @@ import pandas as pd
 
 OperationsSchema = DataFrameSchema({
 
-    # Identifiant
-    "operation_id": Column(int, nullable=False),
+    "operation_id": pa.Column(int, nullable=False),
 
-    # Colonnes texte
-    "type_operation": Column(pa.String, nullable=True),
-    "pourquoi_alerte": Column(pa.String, nullable=True),
-    "moyen_alerte": Column(pa.String, nullable=True),
-    "qui_alerte": Column(pa.String, nullable=True),
-    "categorie_qui_alerte": Column(pa.String, nullable=True),
-    "cross": Column(pa.String, nullable=True),
-    "departement": Column(pa.String, nullable=True),
-    "evenement": Column(pa.String, nullable=True),
-    "categorie_evenement": Column(pa.String, nullable=True),
-    "autorite": Column(pa.String, nullable=True),
-    "seconde_autorite": Column(pa.String, nullable=True),
-    "zone_responsabilite": Column(pa.String, nullable=True),
-    "vent_direction_categorie": Column(pa.String, nullable=True),
-    "cross_sitrep": Column(pa.String, nullable=True),
-    "fuseau_horaire": Column(pa.String, nullable=True),
-    "systeme_source": Column(pa.String, nullable=True),
+    "type_operation": pa.Column(pa.String, nullable=True),
+    "pourquoi_alerte": pa.Column(pa.String, nullable=True),
+    "moyen_alerte": pa.Column(pa.String, nullable=True),
+    "qui_alerte": pa.Column(pa.String, nullable=True),
+    "categorie_qui_alerte": pa.Column(pa.String, nullable=True),
+    "cross": pa.Column(pa.String, nullable=True),
+    "departement": pa.Column(pa.String, nullable=True),
+    "evenement": pa.Column(pa.String, nullable=True),
+    "categorie_evenement": pa.Column(pa.String, nullable=True),
+    "autorite": pa.Column(pa.String, nullable=True),
+    "seconde_autorite": pa.Column(pa.String, nullable=True),
+    "zone_responsabilite": pa.Column(pa.String, nullable=True),
+    "vent_direction_categorie": pa.Column(pa.String, nullable=True),
+    "cross_sitrep": pa.Column(pa.String, nullable=True),
+    "fuseau_horaire": pa.Column(pa.String, nullable=True),
+    "systeme_source": pa.Column(pa.String, nullable=True),
 
-    # Colonnes numériques
-    "latitude": Column(float, nullable=True, checks=Check.in_range(-90, 90)),
-    "longitude": Column(float, nullable=True, checks=Check.in_range(-180, 180)),
-    "vent_direction": Column(float, nullable=True, checks=Check.in_range(0, 360)),
-    "vent_force": Column(float, nullable=True, checks=Check.ge(0)),
-    "mer_force": Column(float, nullable=True, checks=Check.ge(0)),
+    "latitude": pa.Column(float, nullable=True, checks=Check.in_range(-90, 90)),
+    "longitude": pa.Column(float, nullable=True, checks=Check.in_range(-180, 180)),
+    "vent_direction": pa.Column(float, nullable=True, checks=Check.in_range(0, 360)),
+    "vent_force": pa.Column(float, nullable=True, checks=Check.ge(0)),
+    "mer_force": pa.Column(float, nullable=True, checks=Check.ge(0)),
 
-    # Dates
-    "date_heure_reception_alerte": Column(pa.DateTime, nullable=True),
-    "date_heure_fin_operation": Column(pa.DateTime, nullable=True),
+    "date_heure_reception_alerte": pa.Column(pa.DateTime, nullable=True),
+    "date_heure_fin_operation": pa.Column(pa.DateTime, nullable=True),
 
-    # Autres
-    "numero_sitrep": Column(pa.String, nullable=True),
-    "cross_sitrep": Column(pa.String, nullable=True),
+    "numero_sitrep": pa.Column(pa.String, nullable=True),
 })
 
 
@@ -53,14 +46,14 @@ OperationsSchema = DataFrameSchema({
 
 FlotteursSchema = DataFrameSchema({
 
-    "operation_id": Column(int, nullable=False),
-    "numero_ordre": Column(float, nullable=True),
+    "operation_id": pa.Column(int, nullable=False),
+    "numero_ordre": pa.Column(float, nullable=True),
 
-    "pavillon": Column(pa.String, nullable=True),
-    "resultat_flotteur": Column(pa.String, nullable=True),
-    "type_flotteur": Column(pa.String, nullable=True),
-    "categorie_flotteur": Column(pa.String, nullable=True),
-    "numero_immatriculation": Column(pa.String, nullable=True),
+    "pavillon": pa.Column(pa.String, nullable=True),
+    "resultat_flotteur": pa.Column(pa.String, nullable=True),
+    "type_flotteur": pa.Column(pa.String, nullable=True),
+    "categorie_flotteur": pa.Column(pa.String, nullable=True),
+    "numero_immatriculation": pa.Column(pa.String, nullable=True),
 })
 
 
@@ -70,6 +63,6 @@ FlotteursSchema = DataFrameSchema({
 
 ResultatsHumainSchema = DataFrameSchema({
 
-    "operation_id": Column(int, nullable=False),
-    "resultat_flotteur": Column(pa.String, nullable=True),
+    "operation_id": pa.Column(int, nullable=False),
+    "resultat_flotteur": pa.Column(pa.String, nullable=True),
 })
